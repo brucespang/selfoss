132d4fcdf821c8c04e8da70b5ba9a4e04a08eabc
Bruce Spang <bruce@brucespang.com>
2013-12-23 22:13:07 -0500

    changes to make things more usable

--- public/js/selfoss-events-entries.js
+++ public/js/selfoss-events-entries.js
@@ -6,28 +6,28 @@ selfoss.events.entries = function(e) {
     $('.entry, .entry-title').unbind('click');
 
     // show/hide entry
-    var target = selfoss.isMobile() ? '.entry' : '.entry-title';
+    var target = '.entry';
     $(target).click(function() {
         var parent = ((target == '.entry') ? $(this) : $(this).parent());
-        
+
         if(selfoss.isSmartphone()==false) {
             $('.entry.selected').removeClass('selected');
             parent.addClass('selected');
         }
-        
+
         // prevent event on fullscreen touch
         if(parent.hasClass('fullscreen'))
             return;
-        
+
         var autoMarkAsRead = $('#config').data('auto_mark_as_read')=="1" && parent.hasClass('unread');
-        
+
         // anonymize
         selfoss.anonymize(parent.find('.entry-content'));
-        
+
          // show entry in popup
         if(selfoss.isSmartphone()) {
             location.hash = "show";
-            
+
             // hide nav
             if($('#nav').is(':visible')) {
                 var scrollTop = $(window).scrollTop();
@@ -36,13 +36,13 @@ selfoss.events.entries = function(e) {
                 $(window).scrollTop(scrollTop);
                 $('#nav').hide();
             }
-            
+
             // save scroll position and hide content
             var scrollTop = $(window).scrollTop();
             var content = $('#content');
             $(window).scrollTop(0);
             content.hide();
-            
+
             // show fullscreen
             var fullscreen = $('#fullscreen-entry');
             fullscreen.html('<div id="entrr'+parent.attr('id').substr(5)+'" class="entry fullscreen">'+parent.html()+'</div>');
@@ -53,13 +53,13 @@ selfoss.events.entries = function(e) {
                 fullscreen.lazyLoadImages();
                 fullscreen.find('.entry-loadimages').hide();
             }
-            
+
             // set events for fullscreen
             selfoss.events.entriesToolbar(fullscreen);
-            
+
             // set color of all tags by background color
             fullscreen.find('.entry-tags-tag').colorByBrightness();
-    
+
             // set events for closing fullscreen
             fullscreen.find('.entry, .entry-close').click(function(e) {
                 if(e.target.tagName.toLowerCase()=="a")
@@ -69,7 +69,7 @@ selfoss.events.entries = function(e) {
                 $(window).scrollTop(scrollTop);
                 fullscreen.hide();
             });
-            
+
             // automark as read
             if(autoMarkAsRead) {
                 fullscreen.find('.entry-unread').click();
@@ -77,7 +77,7 @@ selfoss.events.entries = function(e) {
         // open entry content
         } else {
             var content = parent.find('.entry-content');
-            
+
             // show/hide (with toolbar)
             if(content.is(':visible')) {
                 parent.find('.entry-toolbar').hide();
@@ -86,41 +86,41 @@ selfoss.events.entries = function(e) {
                 content.show();
                 selfoss.events.entriesToolbar(parent);
                 parent.find('.entry-toolbar').show();
-                
+
                 // automark as read
                 if(autoMarkAsRead) {
                     parent.find('.entry-unread').click();
                 }
             }
-            
+
             // load images not on mobile devices
             if(selfoss.isMobile()==false || $('#config').data('load_images_on_mobile')=="1") {
                 content.lazyLoadImages();
             }
-        } 
+        }
     });
 
     // no source click
     if(selfoss.isSmartphone())
         $('.entry-source, .entry-icon').unbind('click').click(function(e) {e.preventDefault(); return false });
-    
+
     // scroll load more
     $(window).unbind('scroll').scroll(function() {
         if($('#content').is(':visible')==false)
             return;
-    
+
         var content = $('#content');
-        if($('.stream-more').length > 0 
-           && $('.stream-more').position().top < $(window).height() + $(window).scrollTop() 
+        if($('.stream-more').length > 0
+           && $('.stream-more').position().top < $(window).height() + $(window).scrollTop()
            && $('.stream-more').hasClass('loading')==false)
             $('.stream-more').click();
     });
-    
+
     // more
     $('.stream-more').unbind('click').click(function () {
         var streamMore = $(this);
         selfoss.filter.offset += selfoss.filter.itemsPerPage;
-        
+
         streamMore.addClass('loading');
         $.ajax({
             url: $('base').attr('href'),
@@ -137,10 +137,10 @@ selfoss.events.entries = function(e) {
             }
         });
     });
-    
+
     // set color of all tags by background color
     $('.entry-tags-tag').colorByBrightness();
-    
+
     // click a tag
     if(selfoss.isSmartphone()==false) {
         $('.entry-tags-tag').unbind('click').click(function() {
